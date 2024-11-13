3a0f48d5339952baa9fd83932516c5f46ba241a9
MirzaAthallahSalman <atar.haryanto@gmail.com>
2024-11-13 14:54:17 +0700

    Selesai tutor8

--- mental_health_tracker/lib/widgets/left_drawer.dart
+++ mental_health_tracker/lib/widgets/left_drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:mental_health_tracker/menu.dart';
-import 'package:mental_health_tracker/moodentry_form.dart';
+import 'package:mental_health_tracker/screens/menu.dart';
+import 'package:mental_health_tracker/screens/moodentry_form.dart';
+import 'package:mental_health_tracker/screens/list_moodentry.dart';
 
 class LeftDrawer extends StatelessWidget {
   const LeftDrawer({super.key});
@@ -33,7 +34,7 @@ class LeftDrawer extends StatelessWidget {
                     fontSize: 15,
                     fontWeight: FontWeight.normal,
                     color: Colors.white,
-                  ),
+                  )
                 ),
               ],
             ),
@@ -55,12 +56,23 @@ class LeftDrawer extends StatelessWidget {
             title: const Text('Tambah Mood'),
             // Bagian redirection ke MoodEntryFormPage
             onTap: () {
-              Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => MoodEntryFormPage(),
-                  ));
+                Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const MoodEntryFormPage(),
+                ));
             },
+          ),
+          ListTile(
+              leading: const Icon(Icons.add_reaction_rounded),
+              title: const Text('Daftar Mood'),
+              onTap: () {
+                  // Route menu ke halaman mood
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const MoodEntryPage()),
+                  );
+              },
           ),
         ],
       ),
